403c93120ede05160459da43ea586ecb51b57260
richardmagnusson <richard.pe.magnusson@gmail.com>
2019-01-13 17:41:59 +0100

    changed pay service, modified the design briefly

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-		<title>Smarta Kartor prototyp</title>
+		<title>Lär oss kategorisera smartare!</title>
 		<meta charset="utf-8" />
 	</head>
 	<body>
 		<div class="intro-text">
 			<h1>Välkommen!</h1>
-			<h2>Introduktion: </h2>
-			<p>Detta testet går ut på att hitta vilka kategorier som hör ihop med vilka tjänster. När testet startar kommer du att få en tjänst och får sedan i uppgift att klicka på vilken kategori som denna tjänst tillhör. I en del av testet kommer kategorierna stå i textform och ha olika teman så som Underhållning eller Hälsa. I den andra delen av testet kommer kategorierna ej vara i text eller beröra något tema. Du kommer istället bli presenterad ett antal kartor som föreställer olika platser av olika storlekar så som en planritning av ett hem eller en karta över världen. Din uppgift blir då att fundera i linje med “Var hör denna tjänsten hemma”? Sedan ska du välja den karta som du tycker bäst passar ihop med den tjänsten som du har givits. Lycka till!</p>
-			<p>Tryck här för att påbörja testet: <a href="interface.php?session_id=<?php echo uniqid(); ?>&version=<?php echo (rand(0,1) == 1 ? "category" : "map"); ?>&cycle=initial">Start</a></p>
+			<h2>Introduktion</h2>
+			<p>I det här testet kommer du få para ihop tjänster med kategorier. En uppsättning tjänster kommer presenteras för dig. Din uppgift är att för varje tjänst klicka på den kategori där du tycker tjänster hör hemma. Testet består av två delar. I den första delen av testet kommer kategorierna bestå av namn, t.ex. 'underhållning' eller 'hälsa'. I den andra delen av testet kommer kategorierna bestå av kartor. Då ska du istället välja den karta du tycker passar bäst för tjänsten. Välj den kategori eller karta du tycker känns bäst. Vilken del av testet du börjar med är slumpmässigt. Testet går inte på tid. Lycka till!</p>
+			<p ><a id="start-button" href="interface.php?session_id=<?php echo uniqid(); ?>&version=<?php echo (rand(0,1) == 1 ? "category" : "map"); ?>&cycle=initial">Starta testet</a></p>
 		</div>
 	</body>
 </html>
